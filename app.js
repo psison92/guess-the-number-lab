0180bb6341069a9b28e8f705d283f42dcf1667a6
psison <sisonpatrick29@yahoo.com>
2022-05-29 13:48:52 -0500

    game works now. fix while condition in getGuess method

--- app.js
+++ app.js
@@ -7,15 +7,34 @@ const game = {
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-        getGuess(guess)
-        prevGuesses.push(guess)
+        console.log(this.secretNum)
+        
+        do {
+        this.prevGuesses.push(this.getGuess())
+        console.log(this.prevGuesses)
+        this.render()
+      } while (this.prevGuesses[this.prevGuesses.length - 1] !== 
+        this.secretNum)
       },
-    getGuess: function(guess) {
-      do {
-      let guess = prompt(`Enter a guess between ${this.smallestNum}
-        and ${this.biggestNum}.`,'')
-      guess = parseInt(guess)
-    } while (guess !== this.secretNum)
-  }
+    getGuess: function() {
+      let guess 
+      do {  
+        guess = parseInt(prompt(`Enter a guess between ${this.smallestNum}
+          and ${this.biggestNum}.`,''))
+      }
+        while (guess < this.smallestNum || guess > this.biggestNum)
+      return guess
+    },
+    render: function() {
+      if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+        alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)
+      } else if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum) {
+      alert(`Your guess is too low. Previous guesses: ${this.prevGuesses.join()}`)
+      } else if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
+      alert(`Your guess is too high. Previous guesses: ${this.prevGuesses.join()}`)
+      }
+    }
 }
+game.play()
+
 
